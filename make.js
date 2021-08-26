22a553c25ede8686273dbebf7e0a3dd73f4d7eec
Cameron Sajedi <cameronsajedi@gmail.com>
2021-08-26 14:34:18 -0400

    badge creation and issuance scripts

---
+++ make.js
@@ -0,0 +1,68 @@
+const {BN, Long, bytes, units} = require('@zilliqa-js/util');
+const {Zilliqa} = require('@zilliqa-js/zilliqa');
+const {
+    toBech32Address,
+    getAddressFromPrivateKey,
+} = require('@zilliqa-js/crypto');
+
+
+async function main() {
+    const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
+    const CHAIN_ID = 333;
+    const MSG_VERSION = 1;
+    const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
+    privkey = '3375F915F3F9AE35E6B301B7670F53AD1A5BE15D8221EC7FD5E503F21D3450C8';
+    zilliqa.wallet.addByPrivateKey(
+        privkey
+    );
+    const address = getAddressFromPrivateKey(privkey);
+    console.log("Your account address is:");
+    console.log(`${address}`);
+    const myGasPrice = units.toQa('2000', units.Units.Li); // Gas Price that will be used by all transactions
+
+
+    const nftAddr = "zil17l0u6pu7ddvq9eu94n7utltumnx8svqrlz929j";
+    try {
+        const contract = zilliqa.contracts.at(nftAddr);
+        const callTx = await contract.callWithoutConfirm(
+            'addBadge',
+            [
+                {
+                    vname: 'badgeID',
+                    type: 'Uint32',
+                    value: `1`,
+                },
+                {
+                    vname: 'badgeName',
+                    type: 'String',
+                    value: "PELE Early Adopter",
+                }
+            ],
+            {
+                // amount, gasPrice and gasLimit must be explicitly provided
+                version: VERSION,
+                amount: new BN(0),
+                gasPrice: myGasPrice,
+                gasLimit: Long.fromNumber(10000),
+            }
+        );
+
+        // check the pending status
+        const pendingStatus = await zilliqa.blockchain.getPendingTxn(callTx.id);
+        console.log(`Pending status is: `);
+        console.log(pendingStatus.result);
+
+        // process confirm
+        console.log(`The transaction id is:`, callTx.id);
+        console.log(`Waiting transaction be confirmed`);
+        const confirmedTxn = await callTx.confirm(callTx.id);
+
+        console.log(`The transaction status is:`);
+        console.log(confirmedTxn.receipt);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
